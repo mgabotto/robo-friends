16734a2bd4eac293bfe4c29701699e60c6e70831
Martin <mlgabotto@gmail.com>
2020-11-22 18:14:39 -0300

    First commit

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './Hello.css'
+
+class Hello extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className = 'f1 tc'> Hello World </h1>
+                <p> {this.props.greeting}</p>
+            </div>
+            )
+    }
+}
+
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from './Hello.js'
+import 'tachyons'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Hello greeting={'Hello' + 'React Ninja'}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +17,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
