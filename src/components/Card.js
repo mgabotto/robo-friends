b14829ca4d8c60f3a1322a760b37d8dfb81bd95b
Martin <mlgabotto@gmail.com>
2020-11-23 21:45:42 -0300

    robot component

---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import React, {Component}  from 'react';
+// import ReactDOM from 'react-dom';
+
+class Card extends Component  {
+    render() {
+        return (
+            <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+                <img alt='robot' src={`https://robohash.org/${this.props.id}?100x100`} ></img>
+                <div>
+                    <h2>{this.props.name}</h2>
+                    <p> {this.props.email}</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Card
