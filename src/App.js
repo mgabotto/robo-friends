fed2c451b2770258f89c6655a9d5df52fd5c8a98
Martin <mlgabotto@gmail.com>
2020-12-13 20:45:44 -0300

    Actualizacion a c.220

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React , {Component} from 'react';
+import React, { Component } from 'react';
 import CardList from './components/CardList'
-import {robots} from './robots'
+import { robots } from './robots'
 import SearchBox from './components/SearchBox'
-import Test from './components/Test'
 
 
-class App extends Component  {
-    
+
+class App extends Component {
+
     constructor() {
         super()
         this.state = {
@@ -16,22 +16,18 @@ class App extends Component  {
     }
 
     onSearchChange = (event) => {
-        this.setState({searchField: event.target.value})
-      }
-
-
-
+        this.setState({ searchField: event.target.value })
+    }
 
     render() {
-        const filteredRobots = this.state.robots.filter (robot => {
-          return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
         })
         return (
             <div className='tc'>
                 <h1>RoboFriends</h1>
-                <SearchBox searchChange = {this.onSearchChange} />
+                <SearchBox searchChange={this.onSearchChange} />
                 <CardList robots={filteredRobots} />
-                <Test propiedad={"Ramiro"}/>
             </div>
         )
     }
