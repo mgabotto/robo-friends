fa9c6cf204e70b1032e0e4c58b8a3bfb65114eed
Martin <mlgabotto@gmail.com>
2020-12-16 01:08:00 -0300

    Update 224.

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import React, {Component}  from 'react';
 class Card extends Component  {
     render() {
         return (
-            <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <div className='bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
                 <img alt='robot' src={`https://robohash.org/${this.props.id}?100x100`} ></img>
                 <div>
                     <h2>{this.props.name}</h2>
